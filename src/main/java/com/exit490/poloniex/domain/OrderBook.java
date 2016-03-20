d01d4128d86ae3e07d0cab2f0419f7f34f73f891
Maurício Pimenta <mjbpimenta@gmail.com>
2016-03-20 12:55:53 -0300

    Added bids on order  book.

--- src/main/java/com/exit490/poloniex/domain/OrderBook.java
+++ src/main/java/com/exit490/poloniex/domain/OrderBook.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class OrderBook {
 
     private List<List<BigDecimal>> asks;
+    private List<List<BigDecimal>> bids;
+    private int isFrozen;
 
     public List<List<BigDecimal>> getAsks() {
         return asks;
@@ -13,5 +15,21 @@ public class OrderBook {
 
     public void setAsks(List<List<BigDecimal>> asks) {
         this.asks = asks;
+    }
+
+    public List<List<BigDecimal>> getBids() {
+        return bids;
+    }
+
+    public void setBids(List<List<BigDecimal>> bids) {
+        this.bids = bids;
+    }
+
+    public int getIsFrozen() {
+        return isFrozen;
+    }
+
+    public void setIsFrozen(int isFrozen) {
+        this.isFrozen = isFrozen;
     }
 }
